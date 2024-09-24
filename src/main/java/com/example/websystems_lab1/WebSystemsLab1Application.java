d4db910723b992273d5d41e66c8d692fe8b1e38f
Dandddyy <kovalenko.danyl@gmail.com>
2024-09-24 13:03:01 +0300

    Initial commit

---
+++ src/main/java/com/example/websystems_lab1/WebSystemsLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.websystems_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSystemsLab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSystemsLab1Application.class, args);
+    }
+
+}
